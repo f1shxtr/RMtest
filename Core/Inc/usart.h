ab3765d24fbc291ab440f381ff0d29e2ca6ca705
f1shxtr <f1shxtr@sjtu.edu.cn>
2025-10-02 15:39:36 +0800

    feat 串口1

--- Core/Inc/usart.h
+++ Core/Inc/usart.h
@@ -1,9 +1,9 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file    iwdg.h
+  * @file    usart.h
   * @brief   This file contains all the function prototypes for
-  *          the iwdg.c file
+  *          the usart.c file
   ******************************************************************************
   * @attention
   *
@@ -18,8 +18,8 @@
   */
 /* USER CODE END Header */
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __IWDG_H__
-#define __IWDG_H__
+#ifndef __USART_H__
+#define __USART_H__
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,13 +32,13 @@ extern "C" {
 
 /* USER CODE END Includes */
 
-extern IWDG_HandleTypeDef hiwdg;
+extern UART_HandleTypeDef huart7;
 
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
 
-void MX_IWDG_Init(void);
+void MX_UART7_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
@@ -48,5 +48,5 @@ void MX_IWDG_Init(void);
 }
 #endif
 
-#endif /* __IWDG_H__ */
+#endif /* __USART_H__ */
 
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -89,15 +90,16 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   MX_TIM2_Init();
+  MX_UART7_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim2);
-  HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
+  uint8_t txmsg[] = "RoboMaster";
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1) {
-
+    HAL_UART_Transmit(&huart7,txmsg,10,1000);
+    HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
