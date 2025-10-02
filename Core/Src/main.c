40c6b1edf9c47863de69d955283dab68be7c292c
f1shxtr <f1shxtr@sjtu.edu.cn>
2025-10-02 09:38:55 +0800

    feat PWM

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
+#include <math.h>
+
 #include "tim.h"
 #include "gpio.h"
 
@@ -89,17 +92,16 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start(&htim1);
+  HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    if (__HAL_TIM_GET_COUNTER(&htim1)>5000)
-      HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin,GPIO_PIN_SET);
-    else
-      HAL_GPIO_WritePin(LEDR_GPIO_Port, LEDR_Pin,GPIO_PIN_RESET);
+    uint32_t arr_value = __HAL_TIM_GET_AUTORELOAD(&htim1)+1;
+    uint32_t brightness = arr_value * sinf(4 * HAL_GetTick()/1000.f) - 1;
+    __HAL_TIM_SetCompare(&htim1,TIM_CHANNEL_2,brightness);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
